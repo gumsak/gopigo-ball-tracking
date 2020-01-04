f25c26f4a64b383bf4516e3b5a1ad3c8ceae833f
MB <gumsak@users.noreply.github.com>
2020-01-04 22:56:56 +0100

    start multiprocessing implementation

--- ball_tracking.py
+++ ball_tracking.py
@@ -6,6 +6,10 @@ import cv2
 import imutils
 import time
 
+from multiprocessing.pool import ThreadPool
+from multiprocessing import Process
+import multiprocessing as multip
+
 import gopigo3
 
 # Create an instance of the GoPiGo3 class. GPG will be the GoPiGo3 object.
@@ -89,6 +93,43 @@ def __move_robot__(radius, center, w):
        #gpg.stop()
        print("Stopping")
 
+#set the robot's movements
+#radius, center, w
+def control_robot(radius, center, w):
+    __turn_robot__(center[0], 600)
+    __move_robot__(radius, center, 600)
+
+def set_movement(command):
+    
+    if command == "forward":
+        #gpg.right()
+        pass
+    elif command == "reverse":
+        #gpg.right()
+        pass
+    elif command == "left":
+        #gpg.right()
+        pass
+    elif command == "right":
+        #gpg.right()
+        pass
+    elif command == "stop":
+        #gpg.stop()
+        pass
+    else:
+        #gpg.stop()
+        pass
+
+#set up the parallel processing
+def init_multiprocessing():
+    
+    robot_process = Process(name = 'robot_controller',
+                            group = None,
+                            target = control_robot(),
+                            args = (radius, center, None))
+    
+    return robot_process
+
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
@@ -103,68 +144,76 @@ pts = deque(maxlen=args["buffer"])
 # allow the camera or video file to warm up
 time.sleep(2.0)
 
-# keep looping
-while True:
-    # grab the current frame
-    frame = vs.read()
- 
-    # handle the frame from VideoCapture or VideoStream
-    frame = frame[1] if args.get("video", False) else frame
- 
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
-    if frame is None:
-        break
- 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    #frame = imutils.resize(frame, width=600)
-    #blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
- 
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
-    
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    center = None
+if __name__ == '__main__':
 
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        print("center = " + str(center))
+    #start processing
+    robot_process = init_multiprocessing()
+    robot_process.start()
+
+    # keep looping
+    while True:
+        # grab the current frame
+        frame = vs.read()
+     
+        # handle the frame from VideoCapture or VideoStream
+        frame = frame[1] if args.get("video", False) else frame
+     
+        # if we are viewing a video and we did not grab a frame,
+        # then we have reached the end of the video
+        if frame is None:
+            break
+     
+        # resize the frame, blur it, and convert it to the HSV
+        # color space
+        #frame = imutils.resize(frame, width=600)
+        #blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+     
+        # construct a mask for the color "green", then perform
+        # a series of dilations and erosions to remove any small
+        # blobs left in the mask
+        mask = cv2.inRange(hsv, greenLower, greenUpper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
         
-        # only proceed if the radius meets a minimum size
-        if radius > 10:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius),
-                (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
-            __turn_robot__(center[0], 600)
-            __move_robot__(radius, center, 600)
-
-    # show the frame to our screen
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-
-    # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
-        exit()
-
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        center = None
+    
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # its center
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            
+            #get the ceter of the video
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            print("center = " + str(center))
+            
+            # only proceed if the radius meets a minimum size
+            if radius > 10:
+                # draw the circle and its center on the frame,
+                # then update the list of tracked points
+                cv2.circle(frame, (int(x), int(y)), int(radius),
+                    (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                
+                #__turn_robot__(center[0], 600)
+                #__move_robot__(radius, center, 600)
+    
+        # display the frame on the screen
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+    
+        # if the 'q' key is pressed, stop the loop
+        if key == ord("q"):
+            break
 
 # if we are not using a video file, stop the camera video stream
 if not args.get("video", False):
